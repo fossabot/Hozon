d0d47615c6266a04bb9e3f806c9bc970fc35d80b
Gaspard Bariller <gaspard.bariller@hotmail.fr>
2018-05-01 22:40:23 +0200

    Write file to copy to copyList.txt

--- src/js/copySource.js
+++ src/js/copySource.js
@@ -1,6 +1,6 @@
 function copySource() {
   const {dialog} = require('electron').remote;
-  const {app} = require('electron');
+  const {app} = require('electron').remote;
   const fs = require('fs');
   const path = require('path');
 
@@ -15,6 +15,10 @@ function copySource() {
   //List files
   copyFrom = copyFrom.toString(); //Converts path to string
   const temp = app.getPath('desktop'); // TEMP
+  //Remove file
+  //fs.unlink(temp + '/copyList.txt');
+  const listFiles = fs.createWriteStream(temp + '/copyList.txt', {'flags': 'a'});
+
   function walk(copyFrom) {
     fs.readdirSync(copyFrom).forEach(file => {
 
@@ -23,7 +27,7 @@ function copySource() {
       if (fs.lstatSync(fullPath).isDirectory()) {
         walk(fullPath);
       } else {
-        fs.writeFile(temp + 'copyList.txt');
+        listFiles.write(fullPath);
         console.log(fullPath);
       }
     });
